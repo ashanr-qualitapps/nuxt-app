d6153e88e000c1c8f209e33fdd2011ee216dedd6
ashanr-qualitapps <ashan.rajapaksha@qualitapps.com>
2024-07-17 06:57:25 +0000

    npm to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  build: {
+    transpile: ['vue-shared-components']
+  },
+  alias: {
+    '@': resolve(__dirname, './src'),
+    '~~': resolve(__dirname, './src'),
+    '~': resolve(__dirname, './src'),
+    '@@': resolve(__dirname, './'),
+    '@components': resolve(__dirname, './src/components'),
+    '@layouts': resolve(__dirname, './src/layouts'),
+    '@pages': resolve(__dirname, './src/pages'),
+    '@plugins': resolve(__dirname, './src/plugins'),
+    '@static': resolve(__dirname, './src/static'),
+    '@store': resolve(__dirname, './src/store'),
+    '@assets': resolve(__dirname, './src/assets')
+  }
 })
+
+
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Nuxt.js Web Application</h1>
+    <CountryList />
+  </div>
+</template>
+
+<script>
+import { CountryList } from 'vue-shared-components';
+
+export default {
+  components: {
+    CountryList
+  }
+};
+</script>
