ce555ca0a416fd057eaf1ce652e581a8626e35fb
ashanr-qualitapps <ashan.rajapaksha@qualitapps.com>
2024-07-17 07:01:17 +0000

    added nav bar

---
+++ src/pages/countries.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>Country List</h1>
+      <CountryList />
+    </div>
+  </template>
+  
+  <script>
+  import { CountryList } from 'vue-shared-components';
+  
+  export default {
+    components: {
+      CountryList
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    margin-bottom: 20px;
+  }
+  </style>
+  
